01033a198475f44de34533517f0e971948a0a3c9
Sergio <sage_sergio@hotmail.com>
2025-12-16 15:57:59 -0600

    notficicaciones para poryectos

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -5,6 +5,82 @@ const EventEmitter = require('events');
 const notificationController = {};
 const emitter = new EventEmitter();
 
+notificationController.projectsOwnershipNotification = async (req, res) => {  
+    if (!req.body.email)
+      return res.status(400).send({ message: "Correo electrónico faltante" });
+  
+    
+    const query = `
+          SELECT l.nombre as title,
+                l.id_propietario,
+                l.id,
+          FROM public.proyectos AS l
+          INNER JOIN proyectos_usuarios pu ON l.id = pu.proyecto_id
+          WHERE 
+            pu.correo = '${userEmail}'
+            and pu.es_notificado = true
+            AND pu.rol IN ('administrar', 'revisar', 'aporta', 'ver')
+          GROUP BY l.id
+          ORDER BY l.id DESC
+    `;
+
+    try {
+      const { rows } = await databasePool.query({
+        text: query
+      });
+  
+      return res.status(200).send({
+        total: rows.length,
+        projects: rows
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({
+        status: "Error",
+        error: error,
+        message: error.message
+      });
+    }
+};
+
+notificationController.projectsOwnershipNotificationDown = async (req, res) => {  
+  if (!req.body.email)
+    return res.status(400).send({ message: "Correo electrónico faltante" });
+  
+  const query = `
+      UPDATE proyectos_usuarios pu
+      SET es_notificado = FALSE
+      FROM public.proyectos l
+      WHERE l.id = pu.proyecto_id
+        AND pu.correo = '${userEmail}'
+        AND pu.es_notificado = TRUE
+        AND pu.rol IN ('administrar', 'revisar', 'aporta', 'ver')
+      returning *
+  `;
+
+  try {
+    const { rows } = await databasePool.query({
+      text: query
+    });
+
+    return res.status(200).send({
+      status: "Notificaciones desactivadas",
+      projects: rows.length
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      status: "Error",
+      error: error,
+      message: error.message
+    });
+  }
+};
+
+
+
+
+
 notificationController.downloadOwnershipNotification = async (req, res) => {
 	if (!req.body.email) return res.status(400).send({ message: "Correo electrónico faltante" });
 
@@ -54,38 +130,6 @@ notificationController.raisingOwnershipNotification = async (req, res) => {
       return res.status(200).send({
         total: rows.length,
         levantamientos: rows
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(400).send({
-        status: "Error",
-        error: error,
-        message: error.message
-      });
-    }
-};
-
-notificationController.projectsOwnershipNotification = async (req, res) => {  
-    if (!req.body.email)
-      return res.status(400).send({ message: "Correo electrónico faltante" });
-  
-    let query = `
-          SELECT 
-              l.*, 
-              l.nombre as title,  
-              l.region as ruta, CONCAT('apidev/', REPLACE(l.imagen,'./','')) as path_media_folder 
-          from proyectos l 
-          where l.id_propietario = '${req.body.email}' and l.es_notificado = true
-      `;
-  
-    try {
-      const { rows } = await databasePool.query({
-        text: query
-      });
-  
-      return res.status(200).send({
-        total: rows.length,
-        projects: rows
       });
     } catch (error) {
       console.log(error);
