0ea4fd2e0a180dc78879dda555a6be6da7d10175
aranzaaguirre <cbi2183010926@izt.uam.mx>
2026-07-16 16:23:38 -0600

    feat: habilitar creación y actualización de aportes

--- routes/levantamientos.routes.js
+++ routes/levantamientos.routes.js
@@ -13,6 +13,7 @@ router.post("/user/list", levantamientosController.listUser);
 
 //router.post("/user/create", levantamientosController.create);
 router.post("/user/create", filer.arrayUpload("media", 10), levantamientosController.createLevantamiento);
+router.put("/user/update/:id", filer.arrayUpload("media", 10), levantamientosController.updateLevantamiento);
 router.delete("/user/register/:id", levantamientosController.deleteLevantamiento);
 
 router.post('/user/register/v2', levantamientosController.getRegisterV2);
